73a0da95aa7d6193030d5174341f3e0e5faf6a58
Z <ouhassel@gmail.com>
2022-02-22 11:31:48 -0500

    uses cookies to reduce loading time

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-var ws = new WebSocket('wss://soloframe.ozziexyz.repl.co');
+var date = new Date();
+var formatter = new Intl.DateTimeFormat('en-US', { timeZone: "America/New_York" });   
+var usDate = formatter.format(date);
 
-ws.onmessage = m=>{
-    var res = JSON.parse(m.data);
-    document.getElementById('frame').src = res.data;
-    var parsed = res.name.split('.')[0].split('_');
-    document.getElementById('title').textContent = parsed[0].replaceAll('-',' ');
-    document.getElementById('artist').textContent = ' by ' + parsed[1].replaceAll('-',' ');
+if (usDate!=window.localStorage.getItem('lastreq')){
+    var ws = new WebSocket('wss://soloframe.ozziexyz.repl.co');
+    
+    ws.onmessage = m=>{
+        var res = JSON.parse(m.data);
+        var parsed = res.name.split('.')[0].split('_');
+
+        window.localStorage.setItem('today', res.data);
+        window.localStorage.setItem('artist', parsed[1].replaceAll('-',' '));
+        window.localStorage.setItem('title', parsed[0].replaceAll('-',' '));
+        window.localStorage.setItem('lastreq', usDate);
+
+        document.getElementById('frame').src = res.data;
+        document.getElementById('title').textContent = parsed[0].replaceAll('-',' ');
+        document.getElementById('artist').textContent = ' by ' + parsed[1].replaceAll('-',' ');
+        ws.close();
+    }
+}
+else{
+    document.getElementById('frame').src = window.localStorage.getItem('today');
+    document.getElementById('title').textContent = window.localStorage.getItem('title');
+    document.getElementById('artist').textContent = ' by ' + window.localStorage.getItem('artist');
 }
